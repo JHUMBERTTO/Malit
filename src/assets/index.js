824f8382ac6a41f23eadccfe4193f8618867b512
Jhumberto <jhumbertto@gmail.com>
2023-02-03 23:56:29 -0600

    Update

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,26 +1,24 @@
 
 import logo from "./logo.svg";
-import quotes from "./quotes.svg";
 import star from "./Star.svg";
 import shield from "./shield.svg"
 import menu from "./menu.svg";
 import close from "./close.svg";
-import arrowUp from "./arrow-up.svg";
 import instagram from "./instagram.svg";
 import exampleMenu from "./exampleMenu.png";
 import exampleMenu2 from "./exampleMenu2.png";
 import menus from "./menus.png"
+import malit from "./malit.png"
 
 export {
+  malit,
   menus,
   exampleMenu2,
   exampleMenu,
   logo,
-  quotes,
   shield,
   star,
   menu,
   close, 
-  arrowUp,
   instagram,
 };
