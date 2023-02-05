679afd9529cee72302ff617238002b7969bf898a
Migue <hector_miguel_moren0_gonzalez@outlook.com>
2023-02-04 23:49:46 -0600

    cambiosmigue2

--- src/components/ExampleMenu2.jsx
+++ src/components/ExampleMenu2.jsx
@@ -6,8 +6,8 @@ const ExampleMenu2 = () => (
   <section id="soluciones" className={`${layout.section} `}>
     <div className={layout.sectionInfo} data-aos="fade-right" data-aos-duration="1200">
       <h2 className={styles.heading2}>Lleva tu negocio al siguiente nivel <br className='sm:block hidden'/></h2>
-    <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-  Contacta con nosotros para mejorar tu negocio y brindar una experiencia de alta calidad a tus clientes. Como expertos en una solución innovadora para restaurantes, ayudaremos a alcanzar tus metas y mejorar tu éxito en el mercado. </p>
+    <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Únete a nosotros y lleva tu negocio al siguiente nivel con nuestro servicio de menús y QR, diseñada para mejorar la eficiencia y la experiencia del cliente en tu restaurante.</p>
+ 
     <Button styles='mt-10'/>
     </div>
     <div className={layout.sectionImg}>
