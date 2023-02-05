bf5238c61a03ddd69f2855f6cbcb568de8f4a98f
Migue <hector_miguel_moren0_gonzalez@outlook.com>
2023-02-04 23:49:10 -0600

    cambiosmigue

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,17 +1,18 @@
 import styles from '../style'
 import Button from './Button'
 
-const CTA = () =>(
-  <section data-aos="zoom-in" data-aos-duration="1500" className= {`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-white-gradient-2 rounded-[20px] box-shadow`}>
+const CTA = () => (
+  <section data-aos="zoom-in" data-aos-duration="1500" className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-white-gradient-2 rounded-[20px] box-shadow`}>
     <div className='flex-1 flex flex-col' >
-      <h2 className={styles.heading2}>Contrata Ya nuestro servicio!</h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Todo lo que necesitas para hacer tu menu digital y crecer tu negocio desde cualquier parte del mundo</p>
+      <h2 className={styles.heading2}>Sobre nosotros</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}> Somos una empresa especializada en soluciones digitales para restaurantes. <br/>
+      Ofrecemos soluciones personalizadas para mejorar la experiencia de  los clientes y aumentar la eficiencia de los negocios.</p> 
     </div>
     <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
-      <Button/>
+      <Button />
     </div>
   </section>
-  )
+)
 
 
 export default CTA
--- src/components/ExampleMenu2.jsx
+++ src/components/ExampleMenu2.jsx
@@ -7,8 +7,7 @@ const ExampleMenu2 = () => (
     <div className={layout.sectionInfo} data-aos="fade-right" data-aos-duration="1200">
       <h2 className={styles.heading2}>Lleva tu negocio al siguiente nivel <br className='sm:block hidden'/></h2>
     <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-    Mejora tu negocio con nosotros. Haz clic en "Contactar" para más info.
-    </p>
+  Contacta con nosotros para mejorar tu negocio y brindar una experiencia de alta calidad a tus clientes. Como expertos en una solución innovadora para restaurantes, ayudaremos a alcanzar tus metas y mejorar tu éxito en el mercado. </p>
     <Button styles='mt-10'/>
     </div>
     <div className={layout.sectionImg}>
