ad5df377da706c7f811ad5917a8ae5a6237635f9
Jhumberto <jhumbertto@gmail.com>
2023-02-06 20:10:32 -0600

    Menu Fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,27 +5,31 @@ import { navLinks } from '../constants'
 const Navbar = () => {
   const [toggle, setToggle] = useState(false)
   return ( 
-    <nav className='w-full flex py-6 items-center navbar'>
-      <img src={logo} alt="Malit" className="w-[150px] h-[50px]"/>
-      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
-        {navLinks.map((nav,index)=>(
-          <li
-            key={nav.id}
-            className={ ` hover:text-orange-600 font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? "mr-0" : "mr-10"} text-black `}>
-            <a href={`#${nav.id}`}>
-              {nav.title}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <div className='sm:hidden flex flex-1 justify-end items center '>
+    <nav className='flex items-center justify-end navbar ' >
+      <div className='w-full flex py-6 items-start justify-between navbar '>
+      <img src={logo} alt="Malit" className="w-[150px] h-[50px] flex"/>
+        <div>
+          <ul className="list-none sm:flex hidden justify-start items-center flex-1">
+            {navLinks.map((nav,index)=>(
+              <li
+                key={nav.id}
+                className={ `hover:text-orange-600 font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? "mr-0" : "mr-10"} text-black `}>
+                <a href={`#${nav.id}`}>
+                  {nav.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className='sm:hidden flex flex-1 justify-end fixed z-[10]'>
         <img src={toggle ? close : menu } 
         alt="menu"
-        className='W-[28px] h-[28px] object-contain'
+        className='W-[28px] h-[28px] object-contain '
         onClick={() => setToggle((prev) => !prev)} />
         <div
         className={`${toggle ? 'flex' : 'hidden'} p-6 glass z-[6] absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
-          <ul className="list-none flex flex-col justify-end items-center flex-1">
+          <ul className="list-none flex flex-col justify-end items-center flex-1 ">
             {navLinks.map((nav,index)=>( 
             <li
               key={nav.id}
