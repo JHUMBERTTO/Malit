49cbcb244552f53912480f551cd477fa052a7936
ClexyT <zombirt67@gmail.com>
2023-05-19 23:30:23 -0600

    update 2 a 'soon' page

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import CustomRoutes from './components/CustomRoutes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <CustomRoutes />
   </BrowserRouter>
 )
